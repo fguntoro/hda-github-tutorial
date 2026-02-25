1198f5394f6fb30954c752bc0f26b342c6470786
Fernando Guntoro <fernando_guntoro@hotmail.com>
2026-02-25 16:17:03 +0000

    change name and reset for tutorial

--- practice.R
+++ practice.R
@@ -4,10 +4,3 @@ n <- 100
 x <- rnorm(n)
 y <- 1.5 * x + rnorm(n, mean = 0, sd = 5)
 df <- data.frame(dose = n, outcome = y)
-
-# 2. Univariate Analysis
-model <- lm(outcome ~ dose, data = df)
-summary(model)
-
-# 3. Plotting
-plot(df$dose, df$outcome)
