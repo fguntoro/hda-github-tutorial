bbb9fcd831d2af73d43236a5bb420f2dd73df636
Fernando Guntoro <fernando_guntoro@hotmail.com>
2026-02-25 15:05:53 +0000

    Add analysis code

--- simulation.R
+++ simulation.R
@@ -5,4 +5,6 @@ x <- rnorm(n)
 y <- 1.5 * x + rnorm(n, mean = 0, sd = 5)
 df <- data.frame(dose = n, outcome = y)
 
-# track this
+# 2. Univariate Analysis
+model <- lm(outcome ~ dose, data = df)
+summary(model)
